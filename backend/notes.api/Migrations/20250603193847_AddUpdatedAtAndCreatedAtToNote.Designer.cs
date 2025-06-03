efe5947f4dd2eb56c3c72c13be4f0d0072e8ff99
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-06-03 22:40:52 +0300

    add migration to include CreatedAt and UpdatedAt fields in Note model with constraints

--- backend/notes.api/Migrations/20250603193847_AddUpdatedAtAndCreatedAtToNote.Designer.cs
+++ backend/notes.api/Migrations/20250603193847_AddUpdatedAtAndCreatedAtToNote.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using notes.api.Data;
@@ -11,9 +12,11 @@ using notes.api.Data;
 namespace notes.api.Migrations
 {
     [DbContext(typeof(NotesDbContext))]
-    partial class NotesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250603193847_AddUpdatedAtAndCreatedAtToNote")]
+    partial class AddUpdatedAtAndCreatedAtToNote
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/notes.api/Migrations/20250603193847_AddUpdatedAtAndCreatedAtToNote.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace notes.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdatedAtAndCreatedAtToNote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Notes",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Notes",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Notes",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Notes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Notes",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+        }
+    }
+}

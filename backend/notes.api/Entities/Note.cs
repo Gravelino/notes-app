ca27b71f57c5d1bd7bf038e6f9e8306fa42cd408
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-06-03 22:40:19 +0300

    add timestamps and default values to Note model fields

--- backend/notes.api/Entities/Note.cs
+++ backend/notes.api/Entities/Note.cs
@@ -2,7 +2,9 @@ namespace notes.api.Entities;
 
 public class Note
 {
-    public Guid Id { get; set; }
-    public required string Title { get; set; }
-    public required string Content { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required string Title { get; set; } = string.Empty;
+    public required string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
